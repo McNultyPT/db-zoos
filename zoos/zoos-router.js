3ce35c30aa3f5d6195fc7a7fb768bd5e9a6d5e82
McNultyPT <mcnultypt@gmail.com>
2019-02-26 12:54:53 -0800

    adding a put request to modify a zoo

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -73,5 +73,30 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    const zooInfo = req.body;
+
+    if (!zooInfo.name)
+        return res.status(400).json({ errorMessage: 'Please provide a name to be updated.' });
+
+    db('zoos')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count > 0) {
+                db('zoos')
+                    .where({ id: req.params.id })
+                    .then(zoo => {
+                        res.status(200).json(zoo);
+                    });
+            } else {
+                res.status(404).json({ errorMessage: 'A zoo with that ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'There was an error while updating the zoo.' });
+        });
+});
+
 module.exports = router;
 
